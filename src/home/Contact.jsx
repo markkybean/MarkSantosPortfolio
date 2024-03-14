fad0c2d2537276b47dcb8bd75367414a4be7ad24
Mark Santos <marksantos320@gmail.com>
2024-03-15 00:56:02 +0800

    added contact form

--- src/home/Contact.jsx
+++ src/home/Contact.jsx
@@ -1,25 +1,74 @@
-import React from 'react'
+import React from 'react';
+import {
+    Card,
+    Input,
+    Button,
+    Typography,
+    Textarea,
+} from "@material-tailwind/react";
 
 function Contact() {
-  return (
-    <section id="contact" className="max-w-[1160px] w-full mx-auto mt-10 px-4 lg:px-0 " >
-        <div data-aos="fade-up" data-aos-duration="1000" className="bg-gradient-to-r pt-10 lg:pt-24 pb-8 lg:pb-10 px-4 lg:px-8 from-[#45174c] to-[#1c164d] rounded-lg flex-col justify-end items-center gap-10 lg:gap-16 w-full">
-            <div className="flex justify-center items-center flex-col">
-                <p className="text-stone-300 text-base lg:text-lg mb-4 lg:mb-6">CONTACT ME</p>
-                <p className="text-white text-2xl lg:text-3xl font-bold mb-6 lg:mb-8 text-center">Got a project? Let's talk!</p>
-                <p className="text-stone-300 font-semibold text-lg lg:text-xl">marksantos320@gmail.com</p>
-            </div>
-            <div className="flex flex-col lg:flex-row justify-between w-full mt-8 lg:mt-12">
-                <p className="text-white font-bold hidden md:block text-lg lg:text-xl">Mark S. Santos</p>
-                <div className="flex justify-between gap-4 lg:gap-8">
-                    <p className="text-white font-semibold text-lg lg:text-xl">Home</p>
-                    <p className="text-white font-semibold text-lg lg:text-xl">Services</p>
-                    <p className="text-white font-semibold text-lg lg:text-xl">Portfolio</p>
+    return (
+        <section id="contact" className="max-w-[1160px] w-full mx-auto mt-10 px-4 lg:px-0">
+            <div data-aos="fade-up" data-aos-duration="1000" className="bg-gradient-to-r pt-10 lg:pt-24 pb-8 lg:pb-10 px-4 lg:px-8 from-[#45174c] to-[#1c164d] rounded-lg flex-col justify-end items-center gap-10 lg:gap-16 w-full">
+                <div className="flex justify-center items-center flex-col">
+                    <p className="text-stone-300 text-base lg:text-lg mb-4 lg:mb-6">CONTACT ME</p>
+
+                    <Card className='mb-10 bg-gray-800' shadow={false}>
+                        <form className="p-10 lg:p-10 mt-4 lg:mt-8 mb-2 lg:mb-4 w-full max-w-screen-md">
+                            <div className="mb-4 flex flex-col gap-4 lg:gap-6">
+                                <Typography variant="h6" color="white" className="-mb-3">
+                                    Your Name
+                                </Typography>
+                                <Input
+                                    size="lg"
+                                    placeholder="Mark Santos"
+                                    required
+                                    className="pe-16 !border-t-blue-gray-200 focus:!border-t-gray-900 text-white"
+                                    labelProps={{
+                                        className: "before:content-none after:content-none text-white",
+                                    }}
+                                />
+                                <Typography variant="h6" color="white" className="-mb-3">
+                                    Your Email
+                                </Typography>
+                                <Input
+                                    size="lg"
+                                    placeholder="name@mail.com"
+                                    required
+                                    className="pe-16 !border-t-blue-gray-200 focus:!border-t-gray-900 text-white"
+                                    labelProps={{
+                                        className: "before:content-none after:content-none text-white",
+                                    }}
+                                />
+                                <Typography variant="h6" color="white" className="-mb-3">
+                                    Message
+                                </Typography>
+                                <Textarea
+                                    type="text"
+                                    size="lg"
+                                    placeholder="Type your message here"
+                                    required
+                                    className="pe-16 pb-16  !border-t-blue-gray-200 focus:!border-t-gray-900 text-white"
+                                    labelProps={{
+                                        className: "before:content-none after:content-none text-white",
+                                    }}
+                                />
+                            </div>
+
+                            <div className="flex justify-center">
+                                <Button className="bg-[#45174c] text-white mt-4 lg:mt-6">
+                                    Send
+                                </Button>
+                            </div>
+                        </form>
+                    </Card>
+                    <p className="text-white text-2xl lg:text-3xl font-bold mb-3 lg:mb-6">Got a project? Let's talk!</p>
+                    <p className="text-stone-300 font-semibold text-lg lg:text-xl">marksantos320@gmail.com</p>
                 </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    );
 }
 
-export default Contact
+export default Contact;
--- src/home/Footer.jsx
+++ src/home/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="py-[64px]" data-aos="fade-up" data-aos-duration="1000">
-        <p className="text-center font-semibold text-white">© 2023 Mark S. Santos. All rights reserved.</p>
+        <p className="text-center font-semibold text-white">© 2024 Mark S. Santos. All rights reserved.</p>
     </footer>
   )
 }
